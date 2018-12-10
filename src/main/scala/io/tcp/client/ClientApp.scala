b8bdd652478c41b9a58cc7f2dbcd15792423db9f
Abu Raihan Md Rahat Hossain <rahat09@yahoo.com>
2018-12-10 01:01:09 +0000

    create ClientApp.scala

---
+++ src/main/scala/io/tcp/client/ClientApp.scala
@@ -0,0 +1,13 @@
+package io.tcp.client
+
+import akka.actor.ActorSystem
+
+object ClientApp extends App {
+  import ClientManager._
+
+  println("Starting TCP Clients")
+  val system = ActorSystem("tcpClientApp")
+
+  val clientManager = system.actorOf(ClientManager.props)
+  clientManager ! WakeUp
+}
